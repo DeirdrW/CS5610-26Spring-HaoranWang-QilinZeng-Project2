0b696502c405a504e4eee36e19741a1451c2fb66
deirdre503 <wang.haoran10@northeastern.edu>
2026-03-24 00:24:28 -0700

    Deploy build with correct base

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/CS5610-26Spring-HaoranWang-QilinZeng-Project2/',
+  plugins: [react()],
+})
