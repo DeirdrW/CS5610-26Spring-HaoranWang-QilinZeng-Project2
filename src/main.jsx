001feca3ece14584e0e9ea5752fd060638ce0349
deirdre503 <wang.haoran10@northeastern.edu>
2026-03-24 02:12:05 -0700

    change to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { SudokuProvider } from './context/SudokuContext'
 import './styles/global.css'
 
-const BASENAME = '/CS5610-26Spring-HaoranWang-QilinZeng-Project2'
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={BASENAME}>
+    <HashRouter>
       <SudokuProvider>
         <App />
       </SudokuProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
